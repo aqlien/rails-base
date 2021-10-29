9ccd70f1205ff23e2e43c9585fcf9a5d86dbb65e
Alex Lien <codefellow@aqlien.com>
2021-10-29 15:45:39 -0700

    basic setup for dataTables multi-select

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
 // Entry point for the build script in your package.json
 import $ from 'jquery'
 import * as bootstrap from 'bootstrap'
+// import { multiselect } from 'bootstrap-multiselect'
+// require('bootstrap-multiselect')( $, null )
+
 
 // allow variables to be accessed in other files
 window.$ = $
--- app/javascript/tables.js
+++ app/javascript/tables.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
-require( 'datatables.net-bs5' )();
+require( 'datatables.net-bs5' )( window, $ );
 window.dataTableSetup = dataTableSetup;
 
 function dataTableSetup(tableSelector, options = {}) {
@@ -35,6 +35,54 @@ function dataTableUninstall(tableSelector) {
 $.fn.dataTable.Api.register('setupSearch', function(){
   api = this
   tableID = $(api.table().node()).attr('id')
+
+  api.columns( $('.searchable_multiselect') ).eq(0).each( function(colIndex){
+    function regexForMultiSelect(valueArray) {
+      function buildRegex(valueArray){
+        regex = "^("
+        counter = 1
+        for (i = 0; i < valueArray.length; i++) {
+          valueArray[i] = valueArray[i].replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")
+          if (counter < valueArray.length) {
+            regex = regex.concat(valueArray[i] + "|")
+          } else {
+            regex = regex.concat(valueArray[i] + ")$")
+          }
+          counter++
+        }
+        return regex
+      }
+      if (valueArray.indexOf("") != -1){
+        return ".*"
+      } else if ((valueArray.indexOf("^$") != -1) && (valueArray.length > 1)) {
+        valueArray.splice(valueArray.indexOf("^$"), 1)
+        return buildRegex(valueArray)
+      } else if (valueArray.indexOf("^$") != -1) {
+        return "^$"
+      } else {
+        return buildRegex(valueArray)
+      }
+    }
+
+    title = $(this.column(colIndex).header()).text()
+    inputID = 'search_'+tableID+'_'+colIndex.toString()
+    input = $('<select/>', {
+      id: inputID,
+      class: "form-control input-sm",
+      style: "width: 100%;"
+    })
+    input.on('click', function(e){ e.stopPropagation(); })
+    input.on('change', function(){
+      val = $(this).val()
+      regex = regexForMultiSelect(val)
+      api.column( colIndex ).search(regex, true, false).draw();
+    });
+    input.append("<option value='^$'>"+title+": (Blank)</option>")
+    $(this.column(colIndex).header()).html(input)
+    this.column(colIndex).data().unique().sort().each(function(dataValue, dataIndex){ $('#'+inputID).append('<option value="'+dataValue+'">'+title+': '+dataValue+'</option>') })
+    input.children('option[value=""]').remove() // remove blank values, use the blank regex (^$) to search for those instead
+    input.attr('multiple', true)
+  });
 
   api.columns( $('.searchable_select') ).eq(0).each( function(colIndex){
     title = $(this.column(colIndex).header()).text()
