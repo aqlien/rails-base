869da33d158fd61348a5f0ce7112d0cb315aa109
Alex Lien <codefellow@aqlien.com>
2021-10-29 09:34:50 -0700

    add dataTables basic column search/filter

--- app/javascript/tables.js
+++ app/javascript/tables.js
@@ -13,6 +13,8 @@ function dataTableSetup(tableSelector, options = {}) {
   columnDefs.push({visible: false, targets: 'hidden'})
   options['columnDefs'] = columnDefs
 
+  options['initComplete'] = function(){ this.api().setupSearch() }
+
   dataTableInstall(tableSelector, options)
   dataTableUninstall(tableSelector)
 }
@@ -29,3 +31,46 @@ function dataTableUninstall(tableSelector) {
     }
   })
 };
+
+$.fn.dataTable.Api.register('setupSearch', function(){
+  api = this
+  tableID = $(api.table().node()).attr('id')
+
+  api.columns( $('.searchable_select') ).eq(0).each( function(colIndex){
+    title = $(this.column(colIndex).header()).text()
+    inputID = 'search_'+tableID+'_'+colIndex.toString()
+    input = $('<select/>', {
+      id: inputID,
+      class: "form-control input-sm",
+      style: "width: 100%;"
+    })
+    input.on('click', function(e){ e.stopPropagation(); })
+    input.on('change', function(){
+      val = $(this).val()
+      if(val != ''){  val = "^" + val + "$"  }
+      api.column( colIndex ).search(val, true, false).draw();
+    });
+    input.append("<option value=''>"+title+": (All)</option>")
+    // input.append("<option value='^.+$'>"+title+": (Any)</option>")
+    // input.append("<option value='^$'>"+title+": (Blank)</option>")
+    $(this.column(colIndex).header()).html(input)
+    this.column(colIndex).data().unique().sort().each(function(dataValue, dataIndex){ $('#'+inputID).append('<option value="'+dataValue+'">'+title+': '+dataValue+'</option>') })
+  });
+
+  api.columns( $('.searchable_text') ).eq(0).each( function(colIndex){
+    title = $(this.column(colIndex).header()).text()
+    inputID = 'search_'+tableID+'_'+colIndex.toString()
+    input = $('<input/>', {
+      id: inputID,
+      type: 'text',
+      placeholder: title,
+      class: "form-control input-sm",
+      style: "width: 95%;"
+    })
+    input.on('click', function(e){ e.stopPropagation(); })
+    input.on('keyup change', function(){
+      api.column( colIndex ).search(this.value).draw();
+    });
+    $(this.column(colIndex).header()).html(input)
+  })
+});
